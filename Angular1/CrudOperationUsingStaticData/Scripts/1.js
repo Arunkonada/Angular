6b64ab90e9a1398bf0765277905969dc780b4868
arun.konada@outlook.com <arun.konada@outlook.com>
2016-04-30 00:37:37 +0530

    sasas

--- Angular1/CrudOperationUsingStaticData/Scripts/1.js
+++ Angular1/CrudOperationUsingStaticData/Scripts/1.js
@@ -4,54 +4,54 @@ angular.module('crudModule', [])
         function($scope) {
             var data = [
                 {
-                     Id: 101, Name: 'Airi Satou',Position: 'Accountant', Office: 'London',Age: 20,StartDate: '2008/11/28',Salary: 162700, NormalEdit: true,
+                     Id: 101, Name: 'Airi Satou',Position: 'Accountant', Office: 'London',Age: 20,StartDate: new Date(2008,11,28),Salary: 162700, NormalEdit: true,
                      InlineEdit:false
                 },
                 {
-                    Id: 102, Name: 'Caesar Vance', Position: 'Chief Executive Officer (CEO)', Office: 'New York', Age: 30, StartDate: '2011/05/03', Salary: 162700, NormalEdit: true,
+                    Id: 102, Name: 'Caesar Vance', Position: 'Chief Executive Officer (CEO)', Office: 'New York', Age: 30, StartDate: new Date(2009, 11, 28), Salary: 162700, NormalEdit: true,
                     InlineEdit: false
                 },
                 {
-                    Id: 103, Name: 'Ashton Cox', Position: 'Software Engineer', Office: 'New York', Age: 40, StartDate: '2011/12/06', Salary: 162700, NormalEdit: true,
+                    Id: 103, Name: 'Ashton Cox', Position: 'Software Engineer', Office: 'New York', Age: 40, StartDate: new Date(2010, 11, 28), Salary: 162700, NormalEdit: true,
                     InlineEdit: false
                 },
                 {
-                    Id: 104, Name: 'Brielle Williamson', Position: 'Junior Technical Author', Office: 'London', Age: 20, StartDate: '2011/06/07', Salary: 162700, NormalEdit: true,
+                    Id: 104, Name: 'Brielle Williamson', Position: 'Junior Technical Author', Office: 'London', Age: 20, StartDate: new Date(2008, 11, 28), Salary: 162700, NormalEdit: true,
                     InlineEdit: false
                 },
                 {
-                    Id: 105, Name: 'Cedric Kelly	', Position: 'Accountant', Office: 'San Francisco', Age: 20, StartDate: '2011/06/07', Salary: 162700, NormalEdit: true,
+                    Id: 105, Name: 'Cedric Kelly	', Position: 'Accountant', Office: 'San Francisco', Age: 20, StartDate: new Date(2012, 11, 28), Salary: 162700, NormalEdit: true,
                     InlineEdit: false
                 },
                 {
-                    Id: 106, Name: 'Bruno Nash', Position: 'Software Engineer', Office: 'San Francisco', Age: 20, StartDate: '2012/03/29', Salary: 162700, NormalEdit: true,
+                    Id: 106, Name: 'Bruno Nash', Position: 'Software Engineer', Office: 'San Francisco', Age: 20, StartDate: new Date(2014, 11, 28), Salary: 162700, NormalEdit: true,
                     InlineEdit: false
                 },
                 {
-                    Id: 107, Name: 'Brenden Wagner', Position: 'Junior Technical Author', Office: 'London', Age: 20, StartDate: '2012/03/29', Salary: 162700, NormalEdit: true,
+                    Id: 107, Name: 'Brenden Wagner', Position: 'Junior Technical Author', Office: 'London', Age: 20, StartDate: new Date(2013, 11, 28), Salary: 162700, NormalEdit: true,
                     InlineEdit: false
                 }
             ];
             $scope.dynamicId = 108;
             $scope.EmployeeData = data;
-            $scope.Add = function() {
-                data.push({
-                    Id: $scope.dynamicId = $scope.dynamicId+1,
-                    Name: $scope.Name,
-                    Position: $scope.Position,
-                    Office: $scope.Office,
-                    Age: $scope.Age,
-                    StartDate: $scope.StartDate,
-                    Salary: $scope.Salary,
-                    NormalEdit: true,
-                    InlineEdit: false
-                });
-                $scope.Name = '';
-                $scope.Position = '';
-                $scope.Office = '';
-                $scope.Age = '';
-                $scope.StartDate = '';
-                $scope.Salary = '';
+            $scope.Add = function () {
+                    data.push({
+                        Id: $scope.dynamicId = $scope.dynamicId + 1,
+                        Name: $scope.Name,
+                        Position: $scope.Position,
+                        Office: $scope.Office,
+                        Age: $scope.Age,
+                        StartDate: $scope.StartDate,
+                        Salary: $scope.Salary,
+                        NormalEdit: true,
+                        InlineEdit: false
+                    });
+                    $scope.Name = '';
+                    $scope.Position = '';
+                    $scope.Office = '';
+                    $scope.Age = '';
+                    $scope.StartDate = '';
+                    $scope.Salary = '';
             };
             $scope.edit = function (id) {
                 var i;
@@ -93,6 +93,13 @@ angular.module('crudModule', [])
                 }
                 $scope.cancel(id);
             };
+            $scope.reset = function(form) {
+                if (form) {
+                    form.$setPristine();
+                    form.$setUntouched();
+                }
+            };
+            $scope.reset();
         });
 
 
