e61de1d611106f51a809e2fa86cf95abb8f6b09c
arun.konada@outlook.com <arun.konada@outlook.com>
2016-05-27 21:09:15 +0530

    added new files

---
+++ Angular.Models/RequestRevenueFilter.cs
@@ -0,0 +1,39 @@
+﻿namespace Angular.Models
+{
+    public class RequestRevenueFilter
+    {
+        /// <summary>
+        ///     The class constructor.Intialized null value for all properties.
+        /// </summary>
+        public RequestRevenueFilter()
+        {
+            Region = null;
+            Rmd = null;
+            Client = null;
+        }
+
+        /// <summary>
+        ///     Region property
+        /// </summary>
+        /// <value>
+        ///     A value tag is used to describe the region name
+        /// </value>
+        public string Region { get; set; }
+
+        /// <summary>
+        ///     Rmd property
+        /// </summary>
+        /// <value>
+        ///     A value tag is used to describe the rmd name
+        /// </value>
+        public string Rmd { get; set; }
+
+        /// <summary>
+        ///     Client property
+        /// </summary>
+        /// <value>
+        ///     A value tag is used to describe the client name
+        /// </value>
+        public string Client { get; set; }
+    }
+}
---
+++ Angular.Repository/SupplierRepository.cs
@@ -0,0 +1,14 @@
+﻿using Angular.Data;
+
+namespace Angular.Repository
+{
+    public class SupplierRepository
+    {
+        private readonly sampleEntities _sampleEntities;
+
+        public SupplierRepository(sampleEntities sampleEntities)
+        {
+            _sampleEntities = sampleEntities;
+        }
+    }
+}
